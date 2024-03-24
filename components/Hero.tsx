92d713e59cb00544ff12eaabe15f1bc1ac607c23
Sayak-Mallick <sayakmallickkv@gmail.com>
2024-03-24 12:54:49 +0530

    Add Hero component to Home page

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+	return (
+		<div className='hero'>
+			<div className='flex-1 pt-36 padding-x'>
+				<h1 className='hero__title'>Find, book, or rent a car - quickly and easily!</h1>
+                <p className='hero__subtitle'>
+                    We offer a wide selection of vehicles for rent, from compact cars to luxury vehicles, at affordable prices.
+                </p>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
